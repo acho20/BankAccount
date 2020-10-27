ee8e0bee606d9716799ad7f28b57346113a569e3
acho20 <acho20@stuy.edu>
2020-10-27 18:04:29 -0400

    added deposit

--- BankAccount.java
+++ BankAccount.java
@@ -20,4 +20,15 @@ public class BankAccount{
  public void setPassword(String newPass){
   password = newPass;
  }
+
+ public boolean deposit(double amount){
+  if (amount < 0){
+   return false;
+  }else{
+   balance += amount;
+   return true; 
+  }
+  
+
+ }
 }
