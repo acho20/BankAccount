797702cfdfc02ee8f92894934693bd76e3bef996
acho20 <acho20@stuy.edu>
2020-10-27 19:07:50 -0400

    added withdraw

--- BankAccount.java
+++ BankAccount.java
@@ -28,7 +28,15 @@ public class BankAccount{
    balance += amount;
    return true; 
   }
-  
+ }
+
+ public boolean withdraw(double amount){
+  if (amount < 0 || amount > balance){
+   return false;
+  }else{
+   balance -= amount;
+   return true;
+  }
 
  }
 }
